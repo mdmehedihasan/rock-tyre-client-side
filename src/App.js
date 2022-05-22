af2e66ce1d04eccae34499a8f9652ef9485521d0
mehedi hasan <mehedicsebu@gmail.com>
2022-05-22 23:31:17 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Login from './Pages/Login/Login';
+import Blog from './Pages/Blog/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Hello From Rock Tyre</h3>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+
+
+      </Routes>
     </div>
   );
 }
