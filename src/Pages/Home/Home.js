020a767e65865b21099402e8b02f70bbcaaeacdc
mehedi hasan <mehedicsebu@gmail.com>
2022-05-23 21:10:46 +0600

    About us and our story section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import About from './About';
+
 import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
 
 
 const Home = () => {
     return (
-        <Banner></Banner>
+        <div>
+            <Banner></Banner>
+            <About></About>
+            <BusinessSummary></BusinessSummary>
+
+        </div>
     );
 };
 
