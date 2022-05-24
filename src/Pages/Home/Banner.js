eabf8ed15b1341eaf2c5607b255cc9fdddeb1884
mehedi hasan <mehedicsebu@gmail.com>
2022-05-24 22:32:48 +0600

    class replaced by className

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -7,55 +7,55 @@ import banner3 from '../../assets/Banner/banner-3.jpg'
 const Banner = () => {
     return (
         <div>
-            <div class="carousel w-full">
+            <div className="carousel w-full">
 
                 <div id="slide2" style={{ background: `url(${banner1})`, backgroundSize: 'cover' }} className="carousel-item relative w-full ">
 
                     <div className=" hero-overlay bg-opacity-0 text-center xl:mt-80 lg:mt-48 md:mt-16 sm:mt-6 text-white">
 
-                        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
 
 
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 
-                        <a href="#slide1" class="btn btn-circle">❮</a>
-                        <a href="#slide3" class="btn btn-circle">❯</a>
-                    </div>
-                </div>
-
-
-
-                <div id="slide3" style={{ background: `url(${banner2})`, backgroundSize: 'cover' }} class="carousel-item relative w-full">
-
-                    <div className=" hero-overlay bg-opacity-0 text-center xl:mt-80 lg:mt-48 md:mt-16 sm:mt-6 text-white">
-
-                        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
-                    </div>
-
-
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" class="btn btn-circle">❮</a>
-                        <a href="#slide4" class="btn btn-circle">❯</a>
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
 
-                <div id="slide4" style={{ background: `url(${banner3})`, backgroundSize: 'cover' }} class="carousel-item relative w-full xl:pb-96 lg:pb-40 md:pb-16 sm:pb-6">
+
+
+                <div id="slide3" style={{ background: `url(${banner2})`, backgroundSize: 'cover' }} className="carousel-item relative w-full">
 
                     <div className=" hero-overlay bg-opacity-0 text-center xl:mt-80 lg:mt-48 md:mt-16 sm:mt-6 text-white">
 
-                        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
 
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" class="btn btn-circle">❮</a>
-                        <a href="#slide1" class="btn btn-circle">❯</a>
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+                <div id="slide4" style={{ background: `url(${banner3})`, backgroundSize: 'cover' }} className="carousel-item relative w-full xl:pb-96 lg:pb-40 md:pb-16 sm:pb-6">
+
+                    <div className=" hero-overlay bg-opacity-0 text-center xl:mt-80 lg:mt-48 md:mt-16 sm:mt-6 text-white">
+
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
                     </div>
                 </div>
             </div>
