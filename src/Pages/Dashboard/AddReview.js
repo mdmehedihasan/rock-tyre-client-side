e2668435fb737724d143c6e04780546a5154f48e
mehedi hasan <mehedicsebu@gmail.com>
2022-05-24 09:37:25 +0600

    review route added and successfully connected with mongodb

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -3,9 +3,9 @@ import { useForm } from 'react-hook-form';
 import { toast, ToastContainer } from 'react-toastify';
 
 const AddReview = () => {
-    const notify = () => toast("Wow! Your item has been added");
+    const notify = () => toast("Wow! Your review has been added");
 
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit, options } = useForm();
 
     const onSubmit = data => {
         console.log(data);
@@ -29,11 +29,79 @@ const AddReview = () => {
             <div className='w-50 flex flex-col items-center justify-center bg-slate-300 py-9'>
                 <h2 className='tex-5xl font-bold pb-6'>Add A Review</h2>
                 <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                    <input className='mb-2 p-2 border-2 border-black rounded-lg' placeholder='Product Name' {...register("name", { required: true, maxLength: 20 })} /> <br />
-                    <textarea className='mb-2 p-2 border-2 border-black rounded-lg' placeholder='Description' {...register("description")} /><br />
-                    <input className='mb-2 p-2 border-2 border-black rounded-lg' placeholder='Quantity' type="number" {...register("quantity")} /><br />
-                    <input className='mb-2 p-2 border-2 border-black rounded-lg' placeholder='Photo URL' type="text" {...register("img")} /><br />
-                    <input className='mb-2 p-2 border-2 border-black rounded-lg' onClick={notify} type="submit" value="Add Review" />
+                    {/* name field */}
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Your Name"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: 'Name is Required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                        </label>
+                    </div>
+
+
+                    {/* description field */}
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Your Description"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("description", {
+                                required: {
+                                    value: true,
+                                    message: 'Name is Required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.description?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                        </label>
+                    </div>
+
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Rating</span>
+                        </label>
+
+                        <select required className="input input-bordered w-full max-w-xs" {...register("rating", {
+                            required: {
+                                value: true,
+                                message: 'Rating is required'
+                            }
+                        })}>
+                            <option value="">Your Rating</option>
+                            <option value="1⭐">1⭐</option>
+                            <option value="2⭐">2⭐</option>
+                            <option value="3⭐">3⭐</option>
+                            <option value="4⭐">4⭐</option>
+                            <option value="5⭐">5⭐</option>
+
+                            <label className="label">
+                                {errors.rating?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                            </label>
+
+                        </select>
+                        <br />
+
+                    </div>
+
+
+                    <input className='mb-2 p-2 border-2 border-black rounded-lg cursor-pointer' onClick={notify} type="submit" value="Add Review" />
                 </form>
                 <ToastContainer />
             </div>
