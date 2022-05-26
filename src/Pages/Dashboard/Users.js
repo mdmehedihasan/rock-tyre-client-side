4c6cc965c5e36b1f45aae4a5a297f27ff93224a3
mehedi hasan <mehedicsebu@gmail.com>
2022-05-27 00:29:19 +0600

    all user load and users route created

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Loading from '../Shared/Loading';
 
 const Users = () => {
+
+    const [users, setUsers, isLoading] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, [0])
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+
     return (
         <div>
-            <h2 className='text-2xl'>All Users</h2>
+            <h2 className='text-2xl'>All Users:{users.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>S/L</th>
+                            <th>Email</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((u, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{u.email}</td>
+                                <td>Blue</td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
