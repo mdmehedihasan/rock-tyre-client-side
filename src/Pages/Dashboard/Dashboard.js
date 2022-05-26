07a89f5294ff7720892bc66e7000ee2e5c3405a0
mehedi hasan <mehedicsebu@gmail.com>
2022-05-26 14:10:40 +0600

    Dashboard width change

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -15,7 +15,7 @@ const Dashboard = () => {
             </div>
             <div class="drawer-side">
                 <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
 
 
                     {/* <!-- Sidebar content here --> */}
