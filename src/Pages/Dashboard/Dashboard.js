dd1562f4f0e0ad45d37b1e1e6fe307d183126a6a
mehedi hasan <mehedicsebu@gmail.com>
2022-05-27 10:36:42 +0600

    admin route only visible for admin

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../Hooks/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
@@ -24,7 +29,7 @@ const Dashboard = () => {
                     <li><Link to="/dashboard">My Order</Link></li>
                     <li><Link to="/dashboard/review">Review</Link></li>
                     <li><Link to="/dashboard/product">Add Product</Link></li>
-                    <li><Link to="/dashboard/users">All Users</Link></li>
+                    {admin && <li><Link to="/dashboard/users">All Users</Link></li>}
 
 
                 </ul>
