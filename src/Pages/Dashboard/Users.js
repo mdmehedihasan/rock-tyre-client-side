3dc150dad3156c78da33322d350505f3a041235d
mehedi hasan <mehedicsebu@gmail.com>
2022-05-27 09:45:28 +0600

    make admin form user done

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react';
-
+import React from 'react';
+import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
-
-    const [users, setUsers, isLoading] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:5000/user')
-            .then(res => res.json())
-            .then(data => setUsers(data));
-    }, [0])
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
     if (isLoading) {
         return <Loading></Loading>
     }
-
-
     return (
         <div>
-            <h2 className='text-2xl'>All Users:{users.length}</h2>
+            <h2 className="text-2xl">All Users:{users.length}</h2>
             <div class="overflow-x-auto">
                 <table class="table w-full">
 
@@ -25,19 +23,17 @@ const Users = () => {
                         <tr>
                             <th>S/L</th>
                             <th>Email</th>
-                            <th>Favorite Color</th>
+                            <th>Role</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            users.map((u, index) => <tr>
-                                <th>{index + 1}</th>
-                                <td>{u.email}</td>
-                                <td>Blue</td>
-                            </tr>)
-                        }
-
-
+                        {users.map((user, index) => <UserRow
+                            key={user._id}
+                            index={index}
+                            user={user}
+                            refetch={refetch}
+                        ></UserRow>)}
                     </tbody>
                 </table>
             </div>
