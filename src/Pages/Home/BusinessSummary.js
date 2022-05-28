c4e9a608f8dfc395cf1153505e0285c714e42d7f
mehedi hasan <mehedicsebu@gmail.com>
2022-05-28 11:04:49 +0600

    our story is updated

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -2,36 +2,60 @@ import React from 'react';
 
 const BusinessSummary = () => {
     return (
-        <div className='px-40 mt-6'>
+        <div id='reviews' className='mb-20 px-40'>
             <h2 className='font-bold uppercase text-center text-2xl text-black py-3'>Our Story</h2>
-            <div className='bg-black w-100 mb-3' style={{ height: '1px' }}></div>
-            <div className="stats shadow flex items-center justify-center px-40">
+            <div className='bg-black w-100 mb-3 ' style={{ height: '1px' }}></div>
 
-                <div className="stat">
-                    <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <div className='grid sm:grid-cols-2 md:grid-cols-2    lg:grid-cols-4 gap-2'>
+
+                <div>
+                    <div class="stat flex flex-col">
+                        <div class="stat-figure text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                            <div class="stat-title text-black font-bold	">Total Likes</div>
+                            <div class="stat-value text-amber-400">25.6K</div>
+                            <div className="stat-desc text-xl">Likes On FB</div>
+                        </div>
+
                     </div>
-                    <div className="stat-title">Downloads</div>
-                    <div className="stat-value">31K</div>
-                    <div className="stat-desc">Jan 1st - Feb 1st</div>
                 </div>
+                <div>
+                    <div class="stat flex flex-col">
+                        <div
+                            class="stat-figure text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
 
-                <div className="stat">
-                    <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+
+                            <div class="stat-title text-black font-bold	">Engineers</div>
+                            <div class="stat-value text-amber-400 ">256+</div>
+                            <div className="stat-desc text-xl">On Duty</div>
+                        </div>
+
                     </div>
-                    <div className="stat-title">New Users</div>
-                    <div className="stat-value">4,200</div>
-                    <div className="stat-desc">↗︎ 400 (22%)</div>
                 </div>
+                <div>
+                    <div class="stat flex flex-col">
+                        <div class="stat-figure text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
 
-                <div className="stat">
-                    <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                            <div className="stat-title text-black font-bold	">Clients</div>
+                            <div className="stat-value text-amber-400">100+</div>
+                            <div className="stat-desc text-xl">Countries</div>
+                        </div>
+
                     </div>
-                    <div className="stat-title">New Registers</div>
-                    <div className="stat-value">1,200</div>
-                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+                <div>
+                    <div class="stat flex flex-col">
+                        <div class="stat-figure text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+
+                            <div className="stat-title text-black font-bold	">Revenue</div>
+                            <div className="stat-value text-amber-400">206M</div>
+                            <div className="stat-desc text-xl">Every Year</div>
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
